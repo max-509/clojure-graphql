7dd4360376cb3fcd2bcaae3bd0c5e951cd085fbf
max509 <123456vershinin@gmail.com>
2021-11-05 12:42:57 +0700

    Init project

---
+++ test/clojure_graphql/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-graphql.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-graphql.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
