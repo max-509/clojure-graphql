8ade6f3495d2f14f06652925b201ce94c8cc5a45
max509 <123456vershinin@gmail.com>
2021-11-13 16:30:59 +0700

    Write demo syntax parses

--- src/clojure_graphql/core.clj
+++ src/clojure_graphql/core.clj
@@ -1,8 +1,46 @@
-(ns clojure-graphql.core)
+(ns clojure-graphql.core
+  (:require [instaparse.core :as insta]))
 
-(def clauses
-  {'create ::create
-   'match ::match})
+; see https://github.com/aroemers/crustimoney
+;(def create-rule
+;  {:clause [ :create ]
+;   :create [ :create-command ]
+;
+;   :left-brace #"("
+;   :right-brace #")"
+;   :create-command #"(create|CREATE)"
+;   })
+
+(def create-rule
+  (insta/parser
+    "clauses = clause (<whitespaces> clause)*
+    clause = create;
+    create = <create-command> <left-brace> labels properties? <right-brace>;
+
+    properties = <left-curly-bracket> property (<whitespaces> property)* <right-curly-bracket>
+    property = word <':'> <whitespaces> data
+
+    labels = label (<whitespaces> label)*;
+    label = <':'> word;
+
+    create-command = ('create'|'CREATE'|'Create') <whitespaces>;
+
+    left-curly-bracket = <whitespaces>? '{' <whitespaces>?;
+    right-curly-bracket = <whitespaces>? '}' <whitespaces>?;
+    left-brace = <whitespaces>? '(' <whitespaces>?;
+    right-brace = <whitespaces>? ')' <whitespaces>?;
+
+    <data> = string | integer | float | boolean | list;
+    list = '[' (string* | integer* | float* | boolean* | list*) ']';
+    string = <'\"'> word <'\"'>;
+    integer = #'-?[0-9]+';
+    float = #'-?([0-9]+.[0-9]+ | (INF | inf) | (NAN | nan))';
+    boolean = 'True' | 'TRUE' | 'true' | 'False' | 'FALSE' | 'false';
+
+    <word> = #'[a-zA-Z]+';
+    <digit> = #'[0-9]';
+    <whitespaces> = #'\\s+';"))
+
 
 ;(defmulti parse-query (fn clause-extractor
 ;                        ([query] (clause-extractor query {}))
--- test/clojure_graphql/core_test.clj
+++ test/clojure_graphql/core_test.clj
@@ -2,6 +2,8 @@
   (:require [clojure.test :refer :all]
             [clojure-graphql.core :refer :all]))
 
+(println (create-rule "create (:Person :Manager {name: \"Emil\" from: \"Sweden\" klout: 99})"))
+
 
 ;(defquery test-query "create (:Person :Manager {:name \"Emil\", :from \"Sweden\", :klout 99}) - [:FRIEND {:duration \"Forever\"}] -> (:Person :Director {:name \"Frank\", :from \"USA\"})")
 
