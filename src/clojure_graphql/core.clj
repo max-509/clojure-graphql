56bd771339bad2eaa0c9c07f851d5406503b6865
max509 <123456vershinin@gmail.com>
2021-11-19 16:27:06 +0700

    Add 'match' clause with 'where' command

--- src/clojure_graphql/core.clj
+++ src/clojure_graphql/core.clj
@@ -6,49 +6,115 @@
 ;  {:clause [ :create ]
 ;   :create [ :create-command ]
 ;
-;   :left-brace #"("
-;   :right-brace #")"
+;   :left-bracket #"("
+;   :right-bracket #")"
 ;   :create-command #"(create|CREATE)"
 ;   })
 
 (def create-rule
   (insta/parser
     "clauses = clause (<whitespaces> clause)*
-    clause = create;
-    create = <create-command> node (relationship node)*;
+    <clause> = create | match
 
-    node = <left-brace> labels? properties? <right-brace>;
-    relationship = <dash> relation <right-arrow> | <left-arrow> relation <dash> | <dash> relation <dash>;
-    relation = <left-square-bracket> label properties? <right-square-bracket>
+    (*----------------------------CLAUSES DESCRIPTION-----------------------*)
+    create = <create-command> patterns
+    match = <match-command> patterns where?
+    (*----------------------------CLAUSES DESCRIPTION-----------------------*)
 
-    properties = <left-curly-bracket> property (<whitespaces> property)* <right-curly-bracket>
-    <property> = word <':'> <whitespaces> data
+    (*----------------------------SUPPORT FOR CLAUSES-------------------------*)
+    where = <where-command> predicates
+    (*----------------------------SUPPORT FOR CLAUSES-------------------------*)
 
-    labels = label (<whitespaces> label)*;
-    <label> = <':'> word;
+    (*----------------------------PATTERN DESCRIPTION-----------------------*)
+    patterns = pattern (',' <whitespaces>? pattern)*
+    pattern = node (relationship node)*
 
-    create-command = ('create'|'CREATE'|'Create') <whitespaces>;
+    node = <left-bracket> variable-name labels properties <right-bracket>
+    relationship = dash relation right-arrow | left-arrow relation dash | dash relation dash
+    relation = <left-square-bracket> variable-name label properties <right-square-bracket> | Epsilon
 
-    <data> = string | integer | float | boolean | list;
-    list = '[' (string* | integer* | float* | boolean* | list*) ']';
-    string = <'\"'> word <'\"'>;
-    integer = #'-?[0-9]+';
-    float = #'-?([0-9]+.[0-9]+ | (INF | inf) | (NAN | nan))';
-    boolean = 'True' | 'TRUE' | 'true' | 'False' | 'FALSE' | 'false';
+    labels = (label)+ | Epsilon
+    label = <':'> word
 
-    right-arrow = <whitespaces>? '->' <whitespaces>?;
-    left-arrow = <whitespaces>? '<-' <whitespaces>?;
-    dash = <whitespaces>? '-' <whitespaces>?;
-    left-curly-bracket = <whitespaces>? '{' <whitespaces>?;
-    right-curly-bracket = <whitespaces>? '}' <whitespaces>?;
-    left-square-bracket = <whitespaces>? '[' <whitespaces>?;
-    right-square-bracket = <whitespaces>? ']' <whitespaces>?;
-    left-brace = <whitespaces>? '(' <whitespaces>?;
-    right-brace = <whitespaces>? ')' <whitespaces>?;
+    properties = <left-curly-bracket> property (<whitespaces> property)* <right-curly-bracket> | Epsilon
+    property = field <':'> <whitespaces> data
+    field = word
+    (*----------------------------PATTERN DESCRIPTION-----------------------*)
 
-    <word> = #'[a-zA-Z]+';
-    <digit> = #'[0-9]';
-    <whitespaces> = #'\\s+';"))
+    (*----------------------------PREDICATES DESCRIPTION----------------------*)
+    predicates = predicate (<whitespaces> boolean-operator predicate)*
+    predicate = not-command? (brackets-predicates | atom-predicate)
+    <brackets-predicates> = <left-bracket> predicate (<whitespaces> boolean-operator predicate)* <right-bracket>
+    <atom-predicate> = label-check | field-check | pattern-check
+
+    label-check = variable-name labels
+    field-check = variable-name <'.'> field <whitespaces> comparing-operator data
+    pattern-check = pattern
+    <comparing-operator> = lt-command
+                        | le-command
+                        | gt-command
+                        | ge-command
+                        | eq-command
+                        | ne-command
+                        | is-null-command
+                        | is-not-null-command
+                        | starts-with-command
+                        | ends-with-command
+                        | contains-command
+                        | like-regex-command
+                        | in-command
+    <boolean-operator> = and-command | or-command | xor-command
+    (*----------------------------PREDICATES DESCRIPTION----------------------*)
+
+    (*----------------------------DATA TYPES-------------------------------*)
+
+    variable-name = word | Epsilon
+    <data> = string | integer | float | boolean | list
+    list = '[' (string* | integer* | float* | boolean* | list*) ']'
+    string = <'\"'> word <'\"'>
+    integer = #'-?[0-9]+'
+    float = #'-?([0-9]+.[0-9]+ | (INF | inf) | (NAN | nan))'
+    boolean = 'True' | 'TRUE' | 'true' | 'False' | 'FALSE' | 'false'
+    (*----------------------------DATA TYPES-------------------------------*)
+
+    (*-------------------------------------SPECIAL CONSTRUCTS--------------------------------*)
+    <right-arrow> = <whitespaces>? '->' <whitespaces>?
+    <left-arrow> = <whitespaces>? '<-' <whitespaces>?
+    <dash> = <whitespaces>? '-' <whitespaces>?
+    left-curly-bracket = <whitespaces>? '{' <whitespaces>?
+    right-curly-bracket = <whitespaces>? '}' <whitespaces>?
+    left-square-bracket = <whitespaces>? '[' <whitespaces>?
+    right-square-bracket = <whitespaces>? ']' <whitespaces>?
+    left-bracket = <whitespaces>? '(' <whitespaces>?
+    right-bracket = <whitespaces>? ')' <whitespaces>?
+    (*-------------------------------------SPECIAL CONSTRUCTS--------------------------------*)
+
+    (*--------------------------------COMMANDS---------------------------*)
+    create-command = <('create' | 'CREATE' | 'Create')> <whitespaces>
+    match-command = <('match' | 'MATCH' | 'Match')> <whitespaces>
+    where-command = <('where' | 'WHERE' | 'Where')> <whitespaces>
+    and-command = <('and' | 'AND' | 'And')> <whitespaces>
+    or-command = <('or' | 'OR' | 'Or')> <whitespaces>
+    xor-command = <('xor' | 'XOR' | 'Xor')> <whitespaces>
+    not-command = <('not' | 'NOT' | 'Not')> <whitespaces>
+    lt-command = <'<'> <whitespaces>
+    le-command = <'<='> <whitespaces>
+    gt-command = <'>'> <whitespaces>
+    ge-command = <'>='> <whitespaces>
+    eq-command = <'='> <whitespaces>
+    ne-command = <'!='> <whitespaces>
+    is-null-command = <('is null' | 'IS NULL' | 'Is null')> <whitespaces>
+    is-not-null-command = <('is not null' | 'IS NOT NULL' | 'Is not null')> <whitespaces>
+    starts-with-command = <('starts with' | 'STARTS WITH' | 'Starts with')> <whitespaces>
+    ends-with-command = <('ends with' | 'ENDS WITH' | 'Ends with')> <whitespaces>
+    contains-command = <('contains' | 'CONTAINS' | 'Contains')> <whitespaces>
+    like-regex-command = <'=~'> <whitespaces>
+    in-command = <('in' | 'IN' | 'In')> <whitespaces>
+    (*--------------------------------COMMANDS---------------------------*)
+
+    <word> = #'[a-zA-Z]+'
+    <digit> = #'[0-9]'
+    <whitespaces> = #'\\s+'"))
 
 
 ;(defmulti parse-query (fn clause-extractor
--- test/clojure_graphql/core_test.clj
+++ test/clojure_graphql/core_test.clj
@@ -2,8 +2,8 @@
   (:require [clojure.test :refer :all]
             [clojure-graphql.core :refer :all]))
 
-(println (create-rule "create (:Person :Manager {name: \"Emil\" from: \"Sweden\" klout: 99})
-create ()"))
+(println (create-rule "create (a:Person:Manager {name: \"Emil\" from: \"Sweden\" klout: 99})-->()
+                      match (a) where a:Person or (a.name = \"Emil\" and a.klout = 99)"))
 
 
 ;(defquery test-query "create (:Person :Manager {:name \"Emil\", :from \"Sweden\", :klout 99}) - [:FRIEND {:duration \"Forever\"}] -> (:Person :Director {:name \"Frank\", :from \"USA\"})")
