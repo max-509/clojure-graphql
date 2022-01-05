b78bd9549d5cfedffe80b963ff6664036e34e3b8
DenisMihailapov <denismihailapov@yandex.ru>
2022-01-05 12:02:58 +0700

    Refactor graphviz_test.clj
    See graphviz_test.clj for details.

--- test/jsongraph/api/graphviz_test.clj
+++ test/jsongraph/api/graphviz_test.clj
@@ -21,28 +21,37 @@
 
 
 
-(deftest get-graphviz-edges-test
+(deftest get-graphviz-edges-in-text-format-test
   (pprint (get-graphviz-edges
     (first (split-json (full-graph :adjacency))))))
 
-(deftest graph-to-graphviz-test
+(deftest graph-to-graphviz-save-test
   (save! (graph-to-graphviz full-graph) (str path-to-images "full-graph.svg") {:format :svg}))
 
-await-for
-(defn wait
-  [sec]
+(defn wait [sec]
   (doseq [x (range sec)]
     (Thread/sleep 1000)
     (print x)))
 
-(deftest vis-test
+
+; Run This For Get visualization
+(def wait-seconds (* 5 60))
+; This test will be active `wait-seconds` seconds
+; If you want stop test you need just stop runtime
+(deftest full-visualization-test
+  (testing "text format")
   (pprint graph-with-edges)
   (println simple)
   (println (graph-to-graphviz graph-with-edges))
+
+  (testing "show and save")
   (show! simple)
   (save! simple (str path-to-images "simple.svg") {:format :svg})
+
   (show!  (graph-to-graphviz full-graph))
   (save! (graph-to-graphviz full-graph) (str path-to-images "full-graph.svg") {:format :svg})
+
   (show!  (graph-to-graphviz graph-with-edges))
   (save! (graph-to-graphviz graph-with-edges) (str path-to-images "graph-with-edges.svg") {:format :svg})
-  (wait 1000))
+
+  (wait wait-seconds))
